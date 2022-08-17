360d362fd20399a72b0a587f149c584bf14cf7a8
Swadip <roysoumo1607@gmail.com>
2022-08-18 01:03:08 +0600

    complete generate encrypt password & save db, basic handling for log in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.signupGetController = (req,res,next) => {
@@ -7,13 +9,16 @@ exports.signupGetController = (req,res,next) => {
 exports.signupPostController = async (req,res,next) => {
   // let {username, email, password, confirmPassword} = req.body; validation er somoy confirm password lagbe. apatoto korchi na.
   let {username, email, password} = req.body;
+
+  try {    
+  let hashedPassword = await bcrypt.hash(password, 11)
+
   let user = new User({
     username,
     email,
-    password
+    password : hashedPassword
   })
 
-  try {
     let createdUser = await user.save()
     console.log(
       `User created successfully! User info:
@@ -27,11 +32,35 @@ exports.signupPostController = async (req,res,next) => {
 }
 
 exports.loginGetController = (req,res,next) => {
-  
+  res.render('../views/pages/auth/login.ejs', {title: 'Log in'})
 }
 
-exports.loginPostController = (req,res,next) => {
-  
+exports.loginPostController = async (req,res,next) => {
+  let {email, password} = req.body
+  try {
+    let user = await User.findOne({email})
+
+    if(!user) {
+      return res.json({
+        message: `User Not Found`
+      })
+    }
+
+    let match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      return res.json({
+        message: `Password didn't Match`
+      })
+    }
+    console.log(
+      `successfully login by:
+      ${user}`
+    );
+      res.render('../views/pages/auth/login.ejs', {title: 'Log in'})
+  } catch (error) {
+    console.log(`Error occured: ${error}`);
+    next(error)
+  }
 }
 
 exports.logoutController = (req,res,next) => {
@@ -45,3 +74,7 @@ exports.logoutController = (req,res,next) => {
 // 13.4 Create Signup View -- etar jonyo views er pages er auth er signup template e kaj kora hoyeche.
 // 13.5 Signup Controller -- signupGetController, signupPostController & database e save rakhar korechi.
 // 13.6 Connect Database and Create User -- db te connection & user er instance create kore model er document hisebe save korechi  
+// 13.7 Hash Password -- signup post controller e bcrypt er kaj kora hoyeche.
+// 13.8 create log in page view -- login.ejs e kaj kora hoyeche.
+// 13.9 Login Controller -- ekhane log in succefully korte pare jeno ekjon user se kaj kora hoyeche.
+// 13.10 Conclusion
